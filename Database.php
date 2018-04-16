595354c64e6d8323ea9c0d29e6fd7511c90b81f0
Unknown <robinbrongers2001@gmail.com>
2018-04-16 11:25:48 +0200

    Uitlijnen
    Ik heb wat geprobeerd met de style van de website. Vooral met de borders

---
+++ Database.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Robin
+ * Date: 10-4-2018
+ * Time: 13:50
+ */
+
+
+$firstname = 'Robin';
+$lastname = 'Brongers';
+
+
+$password = '';
+$username = 'root';
+
+
+$mysqli = new mysqli('127.0.0.1', $username, $password, 'project_fifa');
+
+
+$stmt = mysqli_prepare($mysqli, "INSERT INTO `name` (`first_name`, `last_name`) VALUES ( ?, ? )");
+mysqli_stmt_bind_param($stmt, "ss", $firstname, $lastname);
+mysqli_stmt_execute($stmt);
+
+?>
