4be491d7279c27154b7bdda2561e062a0ee00ac3
Unknown <robinbrongers2001@gmail.com>
2018-04-17 10:27:59 +0200

    Trying to fix some errors

--- Database.php
+++ Database.php
@@ -15,11 +15,25 @@ $password = '';
 $username = 'root';
 
 $connection = mysqli_connect('127.0.0.1', $username, $password, 'project_fifa');
+
+
+$query = "INSERT INTO `name` (`first_name`, `last_name`) VALUES ( ?, ? )";
+$stmt1 = mysqli_prepare($connection, $query);
+
+mysqli_stmt_bind_param($stmt1, 'sssss',$firstname,$lastname);
+
+
+
+
+
+
+
+
+
+
 //$mysqli = new mysqli('127.0.0.1', $username, $password, 'project_fifa');
-
-
-$stmt = mysqli_prepare($connection, "INSERT INTO `name` (`first_name`, `last_name`) VALUES ( ?, ? )");
-mysqli_stmt_bind_param($stmt, "ss", $firstname, $lastname);
-mysqli_stmt_execute($stmt);
+//$stmt = mysqli_prepare($connection, "INSERT INTO `name` (`first_name`, `last_name`) VALUES ( ?, ? )");
+//mysqli_stmt_bind_param($stmt, "ss", $firstname, $lastname);
+//mysqli_stmt_execute($stmt);
 
 ?>
