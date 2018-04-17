14f95dd6c85e37ae03d990b1cabd399275822def
Unknown <robinbrongers2001@gmail.com>
2018-04-17 11:31:42 +0200

    Fixed the errors

--- Database.php
+++ Database.php
@@ -17,10 +17,10 @@ $username = 'root';
 $connection = mysqli_connect('127.0.0.1', $username, $password, 'project_fifa');
 
 
-$query = "INSERT INTO `name` (`first_name`, `last_name`) VALUES ( ?, ? )";
+$query = "INSERT INTO `tbl_players` (`first_name`, `last_name`) VALUES ( ?, ? )";
 $stmt1 = mysqli_prepare($connection, $query);
 
-mysqli_stmt_bind_param($stmt1, 'sssss',$firstname,$lastname);
+mysqli_stmt_bind_param($stmt1, 'ss',$firstname,$lastname);
 
 
 
@@ -32,7 +32,7 @@ mysqli_stmt_bind_param($stmt1, 'sssss',$firstname,$lastname);
 
 
 //$mysqli = new mysqli('127.0.0.1', $username, $password, 'project_fifa');
-//$stmt = mysqli_prepare($connection, "INSERT INTO `name` (`first_name`, `last_name`) VALUES ( ?, ? )");
+//$stmt = mysqli_prepare($connection, "INSERT INTO `tbl_players` (`first_name`, `last_name`) VALUES ( ?, ? )");
 //mysqli_stmt_bind_param($stmt, "ss", $firstname, $lastname);
 //mysqli_stmt_execute($stmt);
 
