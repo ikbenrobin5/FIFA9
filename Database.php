cc9f41ad3aee49964467af06ea3924c25d502bc7
Unknown <robinbrongers2001@gmail.com>
2018-04-17 10:21:43 +0200

    Working on SQL-injection

--- Database.php
+++ Database.php
@@ -14,11 +14,11 @@ $lastname = 'Brongers';
 $password = '';
 $username = 'root';
 
+$connection = mysqli_connect('127.0.0.1', $username, $password, 'project_fifa');
+//$mysqli = new mysqli('127.0.0.1', $username, $password, 'project_fifa');
 
-$mysqli = new mysqli('127.0.0.1', $username, $password, 'project_fifa');
 
-
-$stmt = mysqli_prepare($mysqli, "INSERT INTO `name` (`first_name`, `last_name`) VALUES ( ?, ? )");
+$stmt = mysqli_prepare($connection, "INSERT INTO `name` (`first_name`, `last_name`) VALUES ( ?, ? )");
 mysqli_stmt_bind_param($stmt, "ss", $firstname, $lastname);
 mysqli_stmt_execute($stmt);
 
