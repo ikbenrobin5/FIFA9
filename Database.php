69aa16da777d699808d6e2b78eb6e6167166ba72
Unknown <robinbrongers2001@gmail.com>
2018-04-19 11:14:58 +0200

    Deleted white spaces

--- Database.php
+++ Database.php
@@ -6,29 +6,19 @@
  * Time: 13:50
  */
 
-
 $firstname = 'Robin';
 $lastname = 'Brongers';
-
 
 $password = '';
 $username = 'root';
 
 $connection = mysqli_connect('127.0.0.1', $username, $password, 'project_fifa');
 
-
 $query = "INSERT INTO `tbl_players` (`first_name`, `last_name`) VALUES ( ?, ? )";
 $stmt1 = mysqli_prepare($connection, $query);
 mysqli_stmt_bind_param($stmt1, 'ss',$firstname,$lastname);
 
-
-
-
-
-
-
-
-
+mysqli_stmt_execute($stmt1);
 
 //$mysqli = new mysqli('127.0.0.1', $username, $password, 'project_fifa');
 //$stmt = mysqli_prepare($connection, "INSERT INTO `tbl_players` (`first_name`, `last_name`) VALUES ( ?, ? )");
